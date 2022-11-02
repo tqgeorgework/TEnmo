905fa06e6f9441fcc0131176becab50b6e8da699
Elijah Grantham <elijahrgrantham@gmail.com>
2022-11-02 18:03:29 -0400

    progress on base methods/strucutre

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/MemoryTransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/MemoryTransferDao.java
@@ -3,6 +3,8 @@ package com.techelevator.tenmo.dao;
 import com.techelevator.tenmo.model.Transfer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +18,7 @@ public class MemoryTransferDao implements TransferDao {
     @Override
     public List<Transfer> getTransfers(int id) {
         List<Transfer> transfers = new ArrayList<>();
-        String sql = "SELECT transfer_id, amount, sender_id, receiver_id FROM transfer WHERE sender_id = ? OR receiver_id = ?";
+        String sql = "SELECT transfer_id, amount, sender_id, receiver_id FROM transfer WHERE sender_id = ? OR receiver_id = ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
         while (results.next()) {
             mapRowToTransfer(results);
@@ -27,7 +29,7 @@ public class MemoryTransferDao implements TransferDao {
     @Override
     public Transfer getTransfer(int id) {
         Transfer transfer = null;
-        String sql = "SELECT transfer_id, amount, sender_id, receiver_id FROM transfer WHERE transfer_id = ?";
+        String sql = "SELECT transfer_id, amount, sender_id, receiver_id FROM transfer WHERE transfer_id = ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
         while (results.next()) {
             mapRowToTransfer(results);
@@ -35,8 +37,27 @@ public class MemoryTransferDao implements TransferDao {
         return transfer;
     }
 
+    @Override
+    public Transfer createTransfer(BigDecimal balance, int senderId, int receiverId) {
+        String sql = "INSERT into transfer VALUES (DEFAULT, ?, ?, ?);";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, balance, senderId, receiverId);
+        if (results.next()) {
+           return mapRowToTransfer(results);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void completeTransfer(Transfer transfer) {
+        String sql = "BEGIN TRANSACTION;" +
+                " UPDATE account SET balance = balance - ? WHERE account_id = ?;" +
+                " UPDATE account SET balance = balance + ? WHERE account_id = ?;" +
+                " COMMIT;";
+        jdbcTemplate.queryForRowSet(sql, Transfer.class, transfer.getAmount(), transfer.getSenderId(), transfer.getAmount(), transfer.getReceiverId());
+    }
+
     private Transfer mapRowToTransfer(SqlRowSet results) {
-       Transfer transfer = new Transfer(results.getInt("transfer_id"), results.getBigDecimal("amount"), results.getInt("sender_id"), results.getInt("receiver_id"));
-        return transfer;
+        return new Transfer(results.getInt("transfer_id"), results.getBigDecimal("amount"), results.getInt("sender_id"), results.getInt("receiver_id"));
     }
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -1,10 +1,14 @@
 package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Transfer;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface TransferDao {
 
     List<Transfer> getTransfers(int id);
     Transfer getTransfer(int id);
+    Transfer createTransfer(BigDecimal balance, int senderId, int receiverId);
+    void completeTransfer(Transfer transfer);
 }
