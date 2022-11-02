b12129158050b1b9f2b6e9aad2fa91e86a4e43f5
Thomas George <tqgeorgework@gmail.com>
2022-11-02 13:13:56 -0400

    Uploading TEnmo v0.3

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -6,12 +6,16 @@ import com.techelevator.tenmo.dao.MemoryAccountDao;
 import com.techelevator.tenmo.dao.MemoryTransferDao;
 import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.model.Transfer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
-@RequestMapping("/account")
+@PreAuthorize("isAuthenticated()")
+@RequestMapping("/accounts")
 public class AccountController {
 
     private AccountDao accountDao;
@@ -22,9 +26,20 @@ public class AccountController {
         this.transferDao = new MemoryTransferDao();
     }
 
-  @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public BigDecimal getBalance(@PathVariable int id) {
         return accountDao.getBalance(id);
     }
 
+    @RequestMapping(path = "/{accountId}/transfer/{transferId}", method = RequestMethod.GET)
+    public Transfer getThisTransfer(@PathVariable int accountId, @PathVariable int transferId) {
+        return transferDao.getTransfer(transferId);
+    }
+
+    @RequestMapping(path = "/{accountId}/transfer", method = RequestMethod.GET)
+    public List<Transfer> getTransfer(@PathVariable int accountId) {
+        return transferDao.getTransfers(accountId);
+    }
+
 }
+
